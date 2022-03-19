c90db6e07ce171ce32722f7b1c4df8dfd6768384
Lucas Pomar <lucaspomar@outlook.com>
2022-03-19 17:06:12 -0300

    Criando projeto

---
+++ src/main/java/com/lucaspomar/arknightsapi/ArknightsApiApplication.java
@@ -0,0 +1,13 @@
+package com.lucaspomar.arknightsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArknightsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArknightsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucaspomar/arknightsapi/ArknightsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucaspomar.arknightsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArknightsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
